24a6b953622a41979c6030cf35a214b47401bab2
AlexBeje <alex.bejenaru93@gmail.com>
2023-07-01 09:57:20 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtPage />
+</template>
+
+<script>
+import "element-plus/theme-chalk/dark/css-vars.css";
+import "~/assets/css/tailwind.css";
+import "~/assets/css/dark/index.css";
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div
+      :class="{
+        'theme-light': !darkMode,
+        'theme-dark': darkMode,
+      }"
+      class="h-screen bg-themeBackground p-5"
+    >
+      <h1 class="text-themeText">Nuxt 3 Tailwind Dark Mode Demo</h1>
+      <Toggle v-model="darkMode" off-label="Light" on-label="Dark" />
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import Toggle from '@vueform/toggle'
+  import { useState } from '#app'
+  import { onMounted, watch } from '@vue/runtime-core'
+  
+  type Theme = 'light' | 'dark'
+  
+  const LOCAL_STORAGE_THEME_KEY = 'theme'
+  
+  const darkMode = useState('theme', () => false)
+  
+  const setTheme = (newTheme: Theme) => {
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
+    darkMode.value = newTheme === 'dark'
+  }
+  
+  onMounted(() => {
+    const isDarkModePreferred = window.matchMedia('(prefers-color-scheme: dark)').matches
+  
+    const themeFromLocalStorage = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme
+  
+    if (themeFromLocalStorage) {
+      setTheme(themeFromLocalStorage)
+    } else {
+      setTheme(isDarkModePreferred ? 'dark' : 'light')
+    }
+  })
+  
+  watch(darkMode, (selected) => {
+    setTheme(selected ? 'dark' : 'light')
+  })
+  </script>
+  
+  <style src="@vueform/toggle/themes/default.css"></style>
