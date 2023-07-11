edce95eb2a6ced8ac68bf873af8ffadf94b62fa1
Alex Beje <abejenaru@opendit.com>
2023-07-11 10:15:30 +0200

    IMPROVE: Improved the size of the elements.

--- components/TFT/Builds.vue
+++ components/TFT/Builds.vue
@@ -18,7 +18,7 @@
         :mgWidth="300"
         :mgHeight="300"
         mgCornerBgColor="#18181B"
-        :zoomFactor="0.60"
+        :zoomFactor="0.70"
         :mgBorderWidth="1"
       />
       <div class="p-2 flex flex-col gap-2 select-none">
