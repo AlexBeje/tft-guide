c86949a920cad5553a80f929cee410377ba4ce63
AlexBeje <alex.bejenaru93@gmail.com>
2023-07-13 01:18:27 +0200

    🐛 FIX: added padding

--- components/TFT/Items.vue
+++ components/TFT/Items.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="border-[1px] border-borderLight rounded-md">
+  <div class="border-[1px] border-borderLight rounded-md px-2">
     <img src="public/items/ItemsCheatSheet.png" alt="TFT Items" class="rounded-md" />
   </div>
 </template>
