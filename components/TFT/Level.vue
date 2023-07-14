9deeacb5624eef19ecf359b50e420d6bbd590c2c
AlexBeje <alex.bejenaru93@gmail.com>
2023-07-14 04:10:31 +0200

    👌 IMPROVE: Improved the level section

--- components/TFT/Level.vue
+++ components/TFT/Level.vue
@@ -25,13 +25,14 @@
                 @click="checkToggle(level.id)"
               >
                 <span class="flex gap-2">
-                  Stage
+                  Stage{{ level.stages.length > 0 && "s" }}
                   <el-tag
                     :type="level.checked ? 'info' : ''"
                     effect="dark"
+                    v-for="stage in level.stages"
                   >
                     <strong :class="`${level.checked && 'text-zinc-300'}`">
-                      {{ level.stage }}
+                      {{ stage }}
                     </strong>
                   </el-tag>
                 </span>
@@ -63,6 +64,32 @@
         </el-timeline-item>
       </el-timeline>
     </TFTDropdown>
+    <div class="flex justify-center">
+      <el-table :data="tableData" border style="width: 365px">
+        <el-table-column prop="level" label="Lvl" width="55" />
+        <el-table-column prop="oneCost" label="1*" width="62" />
+        <el-table-column prop="twoCost" label="2*" width="62">
+          <template #default="scope">
+            <span class="text-[#156831]">{{ scope.row.twoCost }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="threeCost" label="3*" width="62">
+          <template #default="scope">
+            <span class="text-[#12407c]">{{ scope.row.threeCost }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="fourCost" label="4*" width="62">
+          <template #default="scope">
+            <span class="text-[#893088]">{{ scope.row.fourCost }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="fiveCost" label="5*" width="62">
+          <template #default="scope">
+            <span class="text-[#b89d27]">{{ scope.row.fiveCost }}</span>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
   <el-backtop
     :right="16"
@@ -86,7 +113,7 @@ interface LevelGuide {
 
 interface Level {
   id: number;
-  stage: string;
+  stages: string[];
   label: string;
   checked: boolean;
   extraInfo?: string;
@@ -103,6 +130,72 @@ const levelGuidesDB = computed(() => props.data);
 
 /** Variables **/
 const levelGuides = ref(levelGuidesDB.value);
+const tableData = [
+  {
+    level: 3,
+    oneCost: "75%",
+    twoCost: "25%",
+    threeCost: "0%",
+    fourCost: "0%",
+    fiveCost: "0%",
+  },
+  {
+    level: 4,
+    oneCost: "55%",
+    twoCost: "30%",
+    threeCost: "15%",
+    fourCost: "0%",
+    fiveCost: "0%",
+  },
+  {
+    level: 5,
+    oneCost: "45%",
+    twoCost: "33%",
+    threeCost: "20%",
+    fourCost: "2%",
+    fiveCost: "0%",
+  },
+  {
+    level: 6,
+    oneCost: "25%",
+    twoCost: "40%",
+    threeCost: "30%",
+    fourCost: "5%",
+    fiveCost: "0%",
+  },
+  {
+    level: 7,
+    oneCost: "19%",
+    twoCost: "30%",
+    threeCost: "35%",
+    fourCost: "15%",
+    fiveCost: "1%",
+  },
+  {
+    level: 8,
+    oneCost: "16%",
+    twoCost: "20%",
+    threeCost: "35%",
+    fourCost: "25%",
+    fiveCost: "4%",
+  },
+  {
+    level: 9,
+    oneCost: "9%",
+    twoCost: "9%",
+    threeCost: "9%",
+    fourCost: "30%",
+    fiveCost: "16%",
+  },
+  {
+    level: 10,
+    oneCost: "5%",
+    twoCost: "10%",
+    threeCost: "20%",
+    fourCost: "40%",
+    fiveCost: "25%",
+  },
+];
 
 /** Refs **/
 const popoverButtonRef = ref();
